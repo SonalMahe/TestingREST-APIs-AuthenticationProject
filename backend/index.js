46c2f9d2fd31a00ba5c889912e8e195fdda80477
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-05-12 11:55:53 +0200

    changes in firebase key n index file

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,22 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+
+
+import admin from "firebase-admin";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  }),
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
