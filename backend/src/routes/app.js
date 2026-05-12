72ad77f4e1022fc90b79b39015aa13764bb81431
Sonal Maheshwari <sonal.obedient@gmail.com>
2026-05-12 11:59:27 +0200

    Update app.js

--- backend/src/routes/app.js
+++ backend/src/routes/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import bookroutes from './routes/app.js';
+import bookroutes from './routes/book.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
